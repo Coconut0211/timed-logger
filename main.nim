281c8d8415f15f9b5087d862994e22f83ed9bf03
Coconut0211 <ilyaserov02@gmail.com>
2025-02-03 21:58:23 +0300

    1

--- main.nim
+++ main.nim
@@ -18,13 +18,18 @@ type
 
 proc calculateNextRolloverTime(whenInterval: char, interval: int): Time =
   ## Вычисляет время следующей ротации.
-  ## 
-  ## now = getTime()
-  ## now + initTimeInterval(<whatInerval::days|hours|minutes|seconds> = interval)
-  ## 
-  ## Если интервал не поддерживается, то вызвать ошибку.
-  ## 
-  ## Рекомендуется использовать case
+  let now = getTime()
+  case whenInterval:
+    of 'D':
+      return now + initTimeInterval(days = interval)
+    of 'H':
+      return now + initTimeInterval(hours = interval)
+    of 'M':
+      return now + initTimeInterval(minutes = interval)
+    of 'S':
+      return now + initTimeInterval(seconds = interval)
+    else:
+      echo "Интервал не поддерживается"
 
 
 proc newTimedRotatingFileHandler(
@@ -47,14 +52,9 @@ proc newTimedRotatingFileHandler(
 
 proc rotateFile(logger: TimedRollingFileHandler) =
   ## Выполняет ротацию файла.
-
-  # Проверьте на пустоту текущий файл logger.currentFile. Если файл есть, то закройте его.
-
-  # Запишите время ротации в переменную, отформатировав время в формате "yyyy'_'MM'_'dd'_'HH'_'mm'_'ss"
-  # Задайте новое имя файла, включающее время ротации
-  # Используйте moveFile для перемещения файла.
-
-  # Задайте новый logger.currentFile
+  logger.currentFile.close()
+  let currentTime =  format(getTime(),"yyyy'_'MM'_'dd'_'HH'_'mm'_'ss")
+  logger.currentFile = open(join([currentTime,logger.filename]), fmAppend)
 
   # Удаляем старые файлы, если их слишком много
   var logFiles = toSeq(walkFiles("*.log"))  # Так можно получить список всех логов, если изначально задано такое расширение.
@@ -74,17 +74,14 @@ proc log(logger: TimedRollingFileHandler, level: Level, args: varargs[string, `$
     logger.rotateFile()
   let message = args.join(" ")  # Объединяем переданные строки в одну
   var fmtStr = logger.fmtStr
-  # В переменной fmtStr хранится то, что пришло в качестве аргумента fmtStr
-  # В нашем случае, это [$date $time][$levelname]
-  # Необходимо заменить в соответствии с принятыми стандартами:
-  # $date -> now.format("yyyy-MM-dd")
-  # $time -> now.format("HH:mm:ss")
-  # $datetime -> now.format("yyyy-MM-dd'T'HH:mm:ss")
-  # $app -> getAppFilename()
-  # $appdir -> getAppFilename().splitFile.dir
-  # $appname -> getAppFilename().splitFile.name
-  # $levelid -> $LevelNames[level][0]
-  # $levelid -> LevelNames[level]
+  fmtStr = fmtStr.replace("$date", now.format("yyyy-MM-dd"))
+  fmtStr = fmtStr.replace("$time", now.format("HH:mm:ss"))
+  fmtStr = fmtStr.replace("$datetime", now.format("yyyy-MM-dd'T'HH:mm:ss"))
+  fmtStr = fmtStr.replace("$app", getAppFilename())
+  fmtStr = fmtStr.replace("$appdir", getAppFilename().splitFile.dir)
+  fmtStr = fmtStr.replace("$appname", getAppFilename().splitFile.name)
+  fmtStr = fmtStr.replace("$levelid", $LevelNames[level][0])
+  fmtStr = fmtStr.replace("$levelname", LevelNames[level])
   logger.currentFile.writeLine(fmtStr & message)  # Записываем данные в файл
   logger.currentFile.flushFile()  # Принудительно освобождаем поток вывода
 
@@ -97,11 +94,13 @@ var logger = newTimedRotatingFileHandler(
     fmtStr="[$date $time][$levelname] "
 )
 
-while true:
+var i = 1
+while i < 20:
   logger.log(lvlDebug, "1")
   logger.log(lvlInfo, "2")
   logger.log(lvlNotice, "3")
   logger.log(lvlWarn, "4")
   logger.log(lvlError, "5")
   logger.log(lvlFatal, "6")
+  i += 1
   sleep(500)
